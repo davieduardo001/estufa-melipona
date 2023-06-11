eeee44c6acb09f1454f899e32e8633d85f7f66d3
davieduardo001 <ed.miranda@tutanota.com>
2023-06-11 11:24:20 -0300

    ...

--- main-code/main-project.ino
+++ main-code/main-project.ino
@@ -1,29 +1,89 @@
-#include <DFRobot_DHT11.h>
-DFRobot_DHT11 DHT;
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-#define DHT11_PIN 10
-#define RELAY_PIN 2
+#define DHTPIN 2
+#define RELAY_PIN 13
+#define DHTTYPE DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
 
 void setup(){
   Serial.begin(9600);
 
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+
   pinMode(RELAY_PIN, OUTPUT); //set the relay
+
+  delayMS = sensor.min_delay / 10000;
+
 }
 
 void loop(){
-  //604800000s one week
-  delay(604800000);
-  int i;
 
+  //START PROGRAM
+  delay(delayMS);
+  Serial.print(F("STARTING THE PROGRAM :D"));
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+
+  //START FIRST CICLE
+  //604800000s one week
+  Serial.print(F("START FIRST WEEK"))
+  delay(604800000);
+
+  //START SECOND CICLE
+  int i;
   for(i=0; i<1008; i++)
   { //144 = times that will repeat in one day  
     delay(900000); //10min=900000s to the next verification
-    DHT.read(DHT11_PIN);
-    Serial.print("umidade:");
-    Serial.println(DHT.humidity);
 
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      Serial.print(F("Temperature: "));
+      Serial.print(event.temperature);
+      Serial.println(F("°C"));
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      Serial.print(event.relative_humidity);
+      Serial.println(F("%"));
+    }
 
-    if(DHT.humidity >= 90)
+    if(event.relative_humidity >= 90)
     {
       digitalWrite(RELAY_PIN, HIGH);
       delay(15000);
